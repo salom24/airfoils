f27bfa23d322b5e8a908e7fa949a9e0f6f6514ae
Alex Salom <alexss94@msn.com>
2021-11-25 23:23:11 +0100

    Changed streams to vectors

--- racket/airfoil.rkt
+++ racket/airfoil.rkt
@@ -6,7 +6,6 @@
 ;; Variables
 (define (get-NACA-input) (display "Choose a 4-digit NACA > ") (read-line))
 ;(writeln (get-NACA-input))
-
 ;(if (and (string? NACA) (= 4 (string-length NACA)) (andmap char-numeric? (string->list NACA))))
 
 (define M 100)
@@ -17,36 +16,47 @@
 (define AOA (degrees->radians 0))
 
 ; X distribution
-(define X-BASE (for/stream ([n (in-inclusive-range 0 M)])
-            (/ (- 1 (cos (/ (* pi n) M))) 2)))
+(define X-BASE
+  (vector-map
+    (lambda (x) (/ (- 1 (cos (/ (* pi x) M))) 2))
+    (list->vector (inclusive-range 0 M))))
 
 ; Y without camber
-(define Y-SIMETRIC (for/stream ([x X-BASE])
-                     (* 5 T (+ (* 0.2969 (sqrt x))
-                               (- (* 0.126 x))
-                               (- (* 0.3516 (expt x 2)))
-                               (* 0.2843 (expt x 3))
-                               (- (* 0.1036 (expt x 4)))))))
+(define Y-SIMETRIC
+  (vector-map
+    (lambda (x)
+      (* 5 T (+ (* 0.2969 (sqrt x))
+		(- (* 0.126 x))
+		(- (* 0.3516 (expt x 2)))
+		(* 0.2843 (expt x 3))
+		(- (* 0.1036 (expt x 4))))))
+    X-BASE))
 
 ; Camber
-(define Y-CAMBER (for/stream ([x X-BASE])
-                   (if (<= x P)
-                       (* H (- (* 2 P x) (sqr x)) (/ 1 (sqr P)))
-                       (* H (/ 1 (sqr (- 1 P))) (+ 1 (- (* 2 P)) (* 2 P x) (- (sqr x)))))))
+(define Y-CAMBER
+  (vector-map
+    (lambda (x)
+      (if (<= x P)
+	(* H (- (* 2 P x) (sqr x)) (/ 1 (sqr P)))
+	(* H (/ 1 (sqr (- 1 P))) (+ 1 (- (* 2 P)) (* 2 P x) (- (sqr x))))))
+    X-BASE))
 
-(define THETA (for/stream ([x X-BASE])
-                (atan (* 2 H (/ 1 (if (<= x P) (sqr P) (sqr (- 1 P)))) (- P x)))))
+(define THETA
+  (vector-map
+    (lambda (x)
+      (atan (* 2 H (/ 1 (if (<= x P) (sqr P) (sqr (- 1 P)))) (- P x))))
+    X-BASE))
 
-(define X-UP (for/stream ([x X-BASE] [yt Y-SIMETRIC] [th THETA])
-               (- x (* yt (sin th)))))
-(define Y-UP (for/stream ([yc Y-CAMBER] [yt Y-SIMETRIC] [th THETA])
-               (+ yc (* yt (cos th)))))
-(define X-LO (for/stream ([x X-BASE] [yt Y-SIMETRIC] [th THETA])
-               (+ x (* yt (sin th)))))
-(define Y-LO (for/stream ([yc Y-CAMBER] [yt Y-SIMETRIC] [th THETA])
-               (- yc (* yt (cos th)))))
+(define X-UP (vector-map (lambda (x yt th) (- x (* yt (sin th))))
+			 X-BASE Y-SIMETRIC THETA))
+(define Y-UP (vector-map (lambda (yc yt th) (+ yc (* yt (cos th))))
+			 Y-CAMBER Y-SIMETRIC THETA))
+(define X-LO (vector-map (lambda (x yt th) (+ x (* yt (sin th))))
+			 X-BASE Y-SIMETRIC THETA))
+(define Y-LO (vector-map (lambda (yc yt th) (- yc (* yt (cos th))))
+			 Y-CAMBER Y-SIMETRIC THETA))
 
-(plot (list (lines (for/stream ([x X-UP] [y Y-UP]) (vector x y)) #:color "red")
-            (lines (for/stream ([x X-LO] [y Y-LO]) (vector x y)) #:color "red")
-            (lines (for/stream ([x X-BASE] [y Y-CAMBER]) (vector x y)) #:color "blue"))
+(plot (list (lines (vector-map (lambda (x y) (vector x y)) X-UP Y-UP) #:color "red")
+            (lines (vector-map (lambda (x y) (vector x y)) X-LO Y-LO) #:color "red")
+            (lines (vector-map (lambda (x y) (vector x y)) X-BASE Y-CAMBER) #:color "blue"))
   #:x-min 0 #:x-max 1 #:y-min -0.3 #:y-max 0.3 #:aspect-ratio (/ 1 0.6))
